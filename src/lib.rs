7e691d67e676085a4480a7507417527ea09f74f7
Justin Moon <mail@justinmoon.com>
2020-05-23 01:02:51 -0500

    Remove https, get it working

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
 //! SOCKS proxy support for Hyper clients
-#![doc(html_root_url="https://docs.rs/hyper-socks/0.4.0")]
+#![doc(html_root_url = "https://docs.rs/hyper-socks/0.4.0")]
 #![warn(missing_docs)]
 
-extern crate socks;
 extern crate hyper;
+extern crate socks;
 
-#[cfg(test)]
-extern crate hyper_openssl;
-
-use hyper::net::{NetworkConnector, HttpStream, HttpsStream, SslClient};
+use hyper::net::{HttpStream, NetworkConnector};
 use socks::{Socks4Stream, Socks5Stream};
 use std::io;
 use std::net::{SocketAddr, ToSocketAddrs};
@@ -25,7 +22,7 @@ impl Socks4HttpConnector {
     /// proxy with the specified userid.
     pub fn new<T: ToSocketAddrs>(proxy: T, userid: &str) -> io::Result<Socks4HttpConnector> {
         Ok(Socks4HttpConnector {
-            addrs: try!(proxy.to_socket_addrs()).collect(),
+            addrs: (proxy.to_socket_addrs())?.collect(),
             userid: userid.to_owned(),
         })
     }
@@ -36,53 +33,13 @@ impl NetworkConnector for Socks4HttpConnector {
 
     fn connect(&self, host: &str, port: u16, scheme: &str) -> hyper::Result<HttpStream> {
         if scheme != "http" {
-            return Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid scheme for HTTP")
-                           .into());
+            return Err(
+                io::Error::new(io::ErrorKind::InvalidInput, "invalid scheme for HTTP").into(),
+            );
         }
 
-        let socket = try!(Socks4Stream::connect(&self.addrs[..], (host, port), &self.userid));
+        let socket = (Socks4Stream::connect(&self.addrs[..], (host, port), &self.userid))?;
         Ok(HttpStream(socket.into_inner()))
-    }
-}
-
-/// A connector that will produce HttpsStreams proxied over a SOCKS4 server.
-#[derive(Debug)]
-pub struct Socks4HttpsConnector<S> {
-    addrs: Vec<SocketAddr>,
-    userid: String,
-    ssl: S,
-}
-
-impl<S: SslClient> Socks4HttpsConnector<S> {
-    /// Creates a new `Socks4HttpsConnector` which will connect to the specified
-    /// proxy with the specified userid, and use the provided SSL implementation
-    /// to encrypt the resulting stream.
-    pub fn new<T: ToSocketAddrs>(proxy: T, userid: &str, ssl: S) -> io::Result<Self> {
-        Ok(Socks4HttpsConnector {
-            addrs: try!(proxy.to_socket_addrs()).collect(),
-            userid: userid.to_owned(),
-            ssl: ssl,
-        })
-    }
-}
-
-impl<S: SslClient> NetworkConnector for Socks4HttpsConnector<S> {
-    type Stream = HttpsStream<S::Stream>;
-
-    fn connect(&self, host: &str, port: u16, scheme: &str) -> hyper::Result<Self::Stream> {
-        if scheme != "http" && scheme != "https" {
-            return Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid scheme for HTTPS")
-                           .into());
-        }
-
-        let socket = try!(Socks4Stream::connect(&self.addrs[..], (host, port), &self.userid));
-        let stream = HttpStream(socket.into_inner());
-
-        if scheme == "http" {
-            Ok(HttpsStream::Http(stream))
-        } else {
-            Ok(HttpsStream::Https(try!(self.ssl.wrap_client(stream, host))))
-        }
     }
 }
 
@@ -97,7 +54,7 @@ impl Socks5HttpConnector {
     /// proxy.
     pub fn new<T: ToSocketAddrs>(proxy: T) -> io::Result<Socks5HttpConnector> {
         Ok(Socks5HttpConnector {
-            addrs: try!(proxy.to_socket_addrs()).collect(),
+            addrs: proxy.to_socket_addrs()?.collect(),
         })
     }
 }
@@ -107,128 +64,45 @@ impl NetworkConnector for Socks5HttpConnector {
 
     fn connect(&self, host: &str, port: u16, scheme: &str) -> hyper::Result<HttpStream> {
         if scheme != "http" {
-            return Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid scheme for HTTP")
-                           .into());
+            return Err(
+                io::Error::new(io::ErrorKind::InvalidInput, "invalid scheme for HTTP").into(),
+            );
         }
 
-        let socket = try!(Socks5Stream::connect(&self.addrs[..], (host, port)));
+        let socket = Socks5Stream::connect(&self.addrs[..], (host, port))?;
         Ok(HttpStream(socket.into_inner()))
-    }
-}
-
-/// A connector that will produce HttpsStreams proxied over a SOCKS5 server.
-#[derive(Debug)]
-pub struct Socks5HttpsConnector<S> {
-    addrs: Vec<SocketAddr>,
-    ssl: S,
-}
-
-impl<S: SslClient> Socks5HttpsConnector<S> {
-    /// Creates a new `Socks5HttpsConnector` which will connect to the specified proxy, and use the
-    /// provided SSL implementation to encrypt the resulting stream.
-    pub fn new<T: ToSocketAddrs>(proxy: T, ssl: S) -> io::Result<Self> {
-        Ok(Socks5HttpsConnector {
-            addrs: try!(proxy.to_socket_addrs()).collect(),
-            ssl: ssl,
-        })
-    }
-}
-
-impl<S: SslClient> NetworkConnector for Socks5HttpsConnector<S> {
-    type Stream = HttpsStream<S::Stream>;
-
-    fn connect(&self, host: &str, port: u16, scheme: &str) -> hyper::Result<Self::Stream> {
-        if scheme != "http" && scheme != "https" {
-            return Err(io::Error::new(io::ErrorKind::InvalidInput, "invalid scheme for HTTPS")
-                           .into());
-        }
-
-        let socket = try!(Socks5Stream::connect(&self.addrs[..], (host, port)));
-        let stream = HttpStream(socket.into_inner());
-
-        if scheme == "http" {
-            Ok(HttpsStream::Http(stream))
-        } else {
-            Ok(HttpsStream::Https(try!(self.ssl.wrap_client(stream, host))))
-        }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use hyper;
-    use hyper_openssl::OpensslClient;
-    use std::io::Read;
-
     use super::*;
+    use hyper;
 
     #[test]
-    fn google() {
-        let connector = Socks4HttpConnector::new("127.0.0.1:8080", "").unwrap();
+    fn bitcoin_rpc() {
+        // You need to provide these yourself in order to run this test
+        let username = "";
+        let password = "";
+        let socks_host_and_port = "";
+        let rpc_host_and_port = "";
+
+        let connector = Socks5HttpConnector::new(socks_host_and_port).unwrap();
         let client = hyper::Client::with_connector(connector);
-        let mut response = client.get("http://www.google.com").send().unwrap();
+
+        let auth = hyper::header::Authorization(hyper::header::Basic {
+            username: username.to_owned(),
+            password: Some(password.to_owned()),
+        });
+        let response = client
+            .post(rpc_host_and_port)
+            .header(auth)
+            .body(
+                r#"{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": []}"#,
+                )
+            .send()
+            .unwrap();
 
         assert!(response.status.is_success());
-        let mut body = vec![];
-        response.read_to_end(&mut body).unwrap();
-    }
-
-    #[test]
-    fn google_ssl_http() {
-        let ssl = OpensslClient::new().unwrap();
-        let connector = Socks4HttpsConnector::new("127.0.0.1:8080", "", ssl).unwrap();
-        let client = hyper::Client::with_connector(connector);
-        let mut response = client.get("http://www.google.com").send().unwrap();
-
-        assert!(response.status.is_success());
-        let mut body = vec![];
-        response.read_to_end(&mut body).unwrap();
-    }
-
-    #[test]
-    fn google_ssl_https() {
-        let ssl = OpensslClient::new().unwrap();
-        let connector = Socks4HttpsConnector::new("127.0.0.1:8080", "", ssl).unwrap();
-        let client = hyper::Client::with_connector(connector);
-        let mut response = client.get("https://www.google.com").send().unwrap();
-
-        assert!(response.status.is_success());
-        let mut body = vec![];
-        response.read_to_end(&mut body).unwrap();
-    }
-
-    #[test]
-    fn google_v5() {
-        let connector = Socks5HttpConnector::new("127.0.0.1:8080").unwrap();
-        let client = hyper::Client::with_connector(connector);
-        let mut response = client.get("http://www.google.com").send().unwrap();
-
-        assert!(response.status.is_success());
-        let mut body = vec![];
-        response.read_to_end(&mut body).unwrap();
-    }
-
-    #[test]
-    fn google_ssl_http_v5() {
-        let ssl = OpensslClient::new().unwrap();
-        let connector = Socks5HttpsConnector::new("127.0.0.1:8080", ssl).unwrap();
-        let client = hyper::Client::with_connector(connector);
-        let mut response = client.get("http://www.google.com").send().unwrap();
-
-        assert!(response.status.is_success());
-        let mut body = vec![];
-        response.read_to_end(&mut body).unwrap();
-    }
-
-    #[test]
-    fn google_ssl_https_v5() {
-        let ssl = OpensslClient::new().unwrap();
-        let connector = Socks5HttpsConnector::new("127.0.0.1:8080", ssl).unwrap();
-        let client = hyper::Client::with_connector(connector);
-        let mut response = client.get("https://www.google.com").send().unwrap();
-
-        assert!(response.status.is_success());
-        let mut body = vec![];
-        response.read_to_end(&mut body).unwrap();
     }
 }
